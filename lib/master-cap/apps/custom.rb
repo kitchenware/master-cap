3016364a99ee91ea11926e0e16ac3673d940a707
Bertrand Paquet <bertrand.paquet@gmail.com>
2013-11-27 23:47:16 +0100

    Add custom apps type

---
+++ lib/master-cap/apps/custom.rb
@@ -0,0 +1,32 @@
+
+require 'master-cap/apps/base'
+
+class AppsCustom < AppsBase
+
+  def initialize cap, name, config
+    super(cap, name, config)
+    [:required_params].each do |x|
+      raise "Please specify :#{x} attr" unless config.key? x
+    end
+  end
+
+  def default_opts
+    map = {
+      :application => name,
+      :user => config[:user] || "deploy",
+    }
+    config[:required_params].each do |x|
+      begin
+        map[x] = @cap.fetch x
+      rescue
+        raise "Please specify param #{x}"
+      end
+    end
+    map
+  end
+
+  def deploy
+    run_sub_cap :deploy
+  end
+
+end

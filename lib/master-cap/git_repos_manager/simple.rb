7282cb0eb46b966bf4f2bd8caf16f6a851e0bca1
Bertrand Paquet <bertrand.paquet@gmail.com>
2013-12-06 12:00:11 +0100

    Force master-cap version if forced in Gemfile.lock

--- lib/master-cap/git_repos_manager/simple.rb
+++ lib/master-cap/git_repos_manager/simple.rb
@@ -10,6 +10,10 @@ class SimpleGitReposManager
     result = {}
     @repos.each do |x|
       result[x[:url]] = x[:ref] if x[:ref]
+      if x[:url].match('master-cap.git')
+        result = File.read('Gemfile.lock').match(/remote:[^\n]+master-cap.git\n\s*revision: ([0-9a-f]+)\n/)
+        result[x[:url]] = result[1] if result
+      end
     end
     result.size == 0 ? nil : result
   end

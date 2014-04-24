2f39d07526f62d808c604b5a7e0a9c149c88efc8
Bertrand Paquet <bertrand.paquet@gmail.com>
2014-04-24 16:51:26 +0200

    Add lot of override features

--- cookbooks/topology/attributes/default.rb
+++ cookbooks/topology/attributes/default.rb
@@ -8,6 +8,17 @@ if File.exist? topology_file
   topology = JSON.parse(File.read(topology_file), :symbolize_names => true)
   node.set[:topology] = topology[:topology]
   node.set[:apps] = topology[:apps] if topology[:apps]
+
+  if topology[:node_override]
+    topology[:node_override].each do |k, v|
+      if v.class == Mash
+        node.set[k] = (node[k] || {}).to_hash.deep_merge(v)
+      else
+        node.set[k] = v
+      end
+    end
+  end
+
 end
 
 if node[:topology_node_name]
@@ -18,6 +29,20 @@ if node[:topology_node_name]
         node.override[k] = (node[k] || {}).to_hash.deep_merge(v)
       else
         node.override[k] = v
+      end
+    end
+  end
+end
+
+if node[:role_override]
+  node.roles.each do |role|
+    if node[:role_override][role]
+      node[:role_override][role].to_hash.each do |k, v|
+        if v.is_a? Mash
+          node.override[k] = (node[k] || {}).to_hash.deep_merge(v)
+        else
+          node.override[k] = v
+        end
       end
     end
   end

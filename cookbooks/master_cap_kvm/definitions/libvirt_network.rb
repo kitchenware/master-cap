d96197a487798f2e3422e2f599b2c54f77d5331e
Bertrand Paquet <bertrand.paquet@gmail.com>
2014-11-18 22:32:03 +0100

    Add libvirt network definitions

---
+++ cookbooks/master_cap_kvm/definitions/libvirt_network.rb
@@ -0,0 +1,24 @@
+
+define :libvirt_network, {
+  :address => nil,
+  :netmask => nil,
+  :bridge => nil,
+  :forward => false,
+} do
+
+  libvirt_network_params = params
+
+  p libvirt_network_params
+
+  content = "<network><name>#{libvirt_network_params[:name]}</name><bridge name='#{libvirt_network_params[:bridge]}'/>"
+  content += "<forward/>" if libvirt_network_params[:forward]
+  content += "<ip address='#{libvirt_network_params[:address]}' netmask='#{libvirt_network_params[:netmask]}'>"
+  content += "</ip>"
+  content += "</network>"
+
+  execute "create libvirt network #{libvirt_network_params[:name]}" do
+    command "echo \"#{content}\" > /tmp/net.xml && virsh net-define /tmp/net.xml && virsh net-autostart #{libvirt_network_params[:name]} && virsh net-start #{libvirt_network_params[:name]} && rm /tmp/net.xml"
+    not_if "virsh net-list | fgrep #{libvirt_network_params[:name]} | fgrep active | fgrep -q yes"
+  end
+
+end

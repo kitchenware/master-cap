973abfda4704a09d71678ed0cd4d3c3acba871de
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-01-09 14:54:06 +0100

    Make ip adress optional in libvirt_network

--- cookbooks/master_cap_kvm/definitions/libvirt_network.rb
+++ cookbooks/master_cap_kvm/definitions/libvirt_network.rb
@@ -10,7 +10,7 @@ define :libvirt_network, {
 
   content = "<network><name>#{libvirt_network_params[:name]}</name><bridge name='#{libvirt_network_params[:bridge]}'/>"
   content += "<forward/>" if libvirt_network_params[:forward]
-  content += "<ip address='#{libvirt_network_params[:address]}' netmask='#{libvirt_network_params[:netmask]}'>"
+  content += "<ip address='#{libvirt_network_params[:address]}' netmask='#{libvirt_network_params[:netmask]}'>" if libvirt_network_params[:address] &&  libvirt_network_params[:netmask]
   content += "</ip>"
   content += "</network>"
 

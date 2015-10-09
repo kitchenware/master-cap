99c8495021d68c02c7f6197c2dc7c6ff042e8378
Bertrand Paquet <bertrand.paquet@gmail.com>
2015-10-09 11:35:30 +0200

    Try to fix networks starts in libvirt networks

--- cookbooks/master_cap_kvm/definitions/libvirt_network.rb
+++ cookbooks/master_cap_kvm/definitions/libvirt_network.rb
@@ -16,11 +16,6 @@ define :libvirt_network, {
   content += "</ip>" if libvirt_network_params[:address] &&  libvirt_network_params[:netmask]
   content += "</network>"
 
-  execute "create libvirt network #{libvirt_network_params[:name]}" do
-    command "echo \"#{content}\" > /tmp/net.xml && virsh net-define /tmp/net.xml && virsh net-autostart #{libvirt_network_params[:name]} && virsh net-start #{libvirt_network_params[:name]} && rm /tmp/net.xml"
-    not_if "virsh net-list | fgrep #{libvirt_network_params[:name]} | fgrep active | fgrep -q yes"
-  end
-
   if libvirt_network_params[:bind_physical]
 
     include_recipe "master_cap_kvm::libvirt_network_hook"
@@ -60,6 +55,11 @@ EOF
       content script
     end
 
+  end
+
+  execute "create libvirt network #{libvirt_network_params[:name]}" do
+    command "echo \"#{content}\" > /tmp/net.xml && virsh net-define /tmp/net.xml && virsh net-autostart #{libvirt_network_params[:name]} && virsh net-start #{libvirt_network_params[:name]} && rm /tmp/net.xml"
+    not_if "virsh net-list | fgrep #{libvirt_network_params[:name]} | fgrep active | fgrep -q yes"
   end
 
 end
